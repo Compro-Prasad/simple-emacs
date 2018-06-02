20587f97ce0988462a68e46c879053e0bc33ffb3
Compro-Prasad <comproprasad@gmail.com>
2018-06-02 15:09:11 +0530

    Fix switch-window

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(test-c company-web elscreen spaceline undo-tree diff-hl multiple-cursors treemacs-projectile which-key company-quickhelp company-irony company-tern company-jedi company helm-projectile helm-ag projectile expand-region web-mode magit-gitflow magithub magit flycheck-clang-analyzer flycheck helm yasnippet-snippets yasnippet hungry-delete spacemacs-theme use-package)))
+   '(switch-window test-c company-web elscreen spaceline undo-tree diff-hl multiple-cursors treemacs-projectile which-key company-quickhelp company-irony company-tern company-jedi company helm-projectile helm-ag projectile expand-region web-mode magit-gitflow magithub magit flycheck-clang-analyzer flycheck helm yasnippet-snippets yasnippet hungry-delete spacemacs-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

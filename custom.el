4ca003e9599427950fbe62b84364a1c440dfad8b
Compro-Prasad <comproprasad@gmail.com>
2018-06-02 14:08:11 +0530

    Lots of changes and removed elc file
    + Better tab support
    + Better search support
    + Added README

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(test-c company-web elscreen spaceline undo-tree diff-hl multiple-cursors treemacs-projectile which-key company-quickhelp company-irony company-tern company-jedi company helm-projectile helm-ag projectile expand-region web-mode magit-gitflow magithub magit flycheck-clang-analyzer flycheck helm yasnippet-snippets yasnippet hungry-delete spacemacs-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-error ((((class color)) (:underline "Red"))))
+ '(flycheck-warning ((((class color)) (:underline "OrangeRed")))))

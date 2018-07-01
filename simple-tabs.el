589dde3f1b5057c3b34209a6f4ddb97310e37fcf
Compro-Prasad <comproprasad@gmail.com>
2018-07-01 11:03:48 +0530

    Magit buffers of a project are deleted when quiting from status buffer

--- simple-tabs.el
+++ simple-tabs.el
@@ -1,3 +1,5 @@
+(require 'rx)
+
 (use-package tabbar-ruler
   :defer t
   :ensure t
@@ -23,8 +25,26 @@
   :config
   (tabbar-ruler-group-by-projectile-project)
   (custom-set-faces
-    '(tabbar-highlight ((t (:background "#272822"))))
     '(tabbar-selected ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :family "Sans Serif"))))
     '(tabbar-selected-modified ((t (:inherit tabbar-default :background "#272822" :foreground "tomato" :box nil :height 150 :family "Sans Serif"))))
     '(tabbar-unselected ((t (:inherit tabbar-selected :background "#444" :foreground "#aaa" :height 160))))
     '(tabbar-unselected-modified ((t (:inherit tabbar-selected-modified :background "#444"))))))
+
+(defun simple-emacs/basename (file)
+  "Returns just the file name of the given FILE."
+  (file-name-nondirectory (directory-file-name file)))
+
+(defun simple-emacs/project-kill-magit ()
+  "Kill current project's magit buffers."
+  (interactive)
+  (kill-matching-buffers (rx
+                          (and line-start
+                               "magit"
+                               (or "" (and "-" (zero-or-more (in "a-z"))))
+                               ": "
+                               (or (eval (simple-emacs/basename default-directory))
+                                   (eval (projectile-project-name)))))
+                         t t)
+  (delete-window))
+
+(define-key magit-status-mode-map (kbd "q") 'simple-emacs/project-kill-magit)

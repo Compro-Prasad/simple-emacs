de55bf4c5cd93de329f126942887fc7fbba52707
Compro-Prasad <comproprasad@gmail.com>
2018-07-28 14:36:38 +0530

    Removed rest of the packages

--- elpa/pipenv-20180402.2127/pipenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pipenv" "20180402.2127" "A Pipenv porcelain." '((emacs "25.1") (f "0.19.0") (s "1.12.0")) :commit "7f1fb3dd1704e535e67860c58080e55fc89e07f4" :url "https://github.com/pwalsh/pipenv.el")
